6fc9b816aa10bd30444d567581ce072f126b4b6d
Pavan Gowda T S <pavangowdats01@gmail.com>
2024-11-14 18:46:30 +0530

    backend 2.0

--- Backend/Routes/product_routes.js
+++ Backend/Routes/product_routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const productRoutes = express.Router();
-const Product  = require('../Models/product_models');
+const Product = require('../Models/product_models');
 
 productRoutes.post('/stocksIn', async (req, res) => {
    const { userEmail, products } = req.body;
@@ -11,7 +11,8 @@ productRoutes.post('/stocksIn', async (req, res) => {
             { userEmail: userEmail, productId: product.productId }, 
             { 
                productName: product.productName,
-               stocksBalance: product.stocksBalance 
+               $inc: { stocksBalance: product.stocksBalance }, 
+               price: product.price,
             },
             { upsert: true, new: true, setDefaultsOnInsert: true } 
          );
@@ -25,7 +26,51 @@ productRoutes.post('/stocksIn', async (req, res) => {
    }
 });
 
+productRoutes.post('/getProduct', async (req, res) => {
+    const { userEmail, productId } = req.body;
+ 
+    try {
+       const foundProduct = await Product.findOne({
+          userEmail: userEmail,
+          productId: productId
+       });
+ 
+       if (foundProduct) {
+          res.status(200).json(foundProduct);
+       } else {
+          res.status(404).json({ message: "Product not found" });
+       }
+    } catch (error) {
+       console.error(error);
+       res.status(500).json({ message: "Error retrieving product data", error });
+    }
+ });
+ 
+
+
+productRoutes.post('/stocksOut', async (req, res) => {
+    const { userEmail, products } = req.body;
+ 
+    try {
+       const productPromises = products.map(product => {
+          return Product.findOneAndUpdate(
+             {
+                userEmail: userEmail,
+                productId: product.productId
+             },
+             {
+                $inc: { stocksBalance: -product.productOut }  
+             },
+             { new: true } 
+          );
+       });
+ 
+       await Promise.all(productPromises); 
+       res.status(200).json({ message: "Stock data decremented successfully" });
+    } catch (error) {
+       console.error(error);
+       res.status(500).json({ message: "Error decrementing stock data", error });
+    }
+ });
+ 
 module.exports = productRoutes;
-
-
-
